a7f8af53e741944f05c6dd1603597dd2313bfc40
Frank Mendez <frankmendezresources@gmail.com>
2016-12-07 14:35:02 +0800

    feat(select2): select2 js

---
+++ gulp/tasks/select2-js-lib.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+module.exports = function() {
+  return gulp.src('node_modules/select2/dist/js/select2.min.js')
+    .pipe(rename('select2.js'))
+    .pipe(gulp.dest('public/dist/css/lib'));
+};
