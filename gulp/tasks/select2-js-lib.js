b0704d803d47873a7f5318d77bea9524eab25838
Frank Mendez <frankmendezresources@gmail.com>
2016-12-07 14:45:48 +0800

    feat(select2): hotfix directory error

--- gulp/tasks/select2-js-lib.js
+++ gulp/tasks/select2-js-lib.js
@@ -4,5 +4,5 @@ var rename = require('gulp-rename');
 module.exports = function() {
   return gulp.src('node_modules/select2/dist/js/select2.min.js')
     .pipe(rename('select2.js'))
-    .pipe(gulp.dest('public/dist/css/lib'));
+    .pipe(gulp.dest('public/dist/js/lib'));
 };
