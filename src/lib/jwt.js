2d6cb34d6c860774dce1418aa8bf07682b7a2167
Roger Madjos <roger.madjos@gmail.com>
2016-12-19 20:57:40 +0800

    feat(auth): Token model

---
+++ src/lib/jwt.js
@@ -0,0 +1,2 @@
+export const JWT_SECRET = 'VZWppFy4R8xuV6wKccMXvDTjH7WnPhfY';
+export const TOKEN_TTL = 1210000000;
---
+++ src/models/token.js
@@ -0,0 +1,38 @@
+import db from '../lib/mongoose';
+import {TOKEN_TTL} from '../lib/jwt';
+import mongoose from 'mongoose';
+import moment from 'moment';
+
+let Schema = mongoose.Schema;
+
+let schema = new Schema({
+  jti: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  createdAt: Date
+});
+
+schema.pre('save', function(next) {
+  this.createdAt = new Date();
+  next();
+});
+
+schema.statics.cleanUp = function * () {
+  yield this
+    .model('User')
+    .remove({createdAt: {
+      $lte: moment(new Date()).subtract(TOKEN_TTL, 'milliseconds').toDate()
+    }})
+    .exec();
+};
+
+schema.index({jti: 1}, {unique: true});
+schema.index({creator: 1});
+schema.index({createdAt: -1});
+
+export let Token = db.model('Token', schema);
