2fa4e54d538919a75a519d28a98ba62c504c31fb
Roger Madjos <roger.madjos@gmail.com>
2016-12-21 17:49:37 +0800

    feat(groups): added members attribute

--- src/models/group.js
+++ src/models/group.js
@@ -3,7 +3,40 @@ import mongoose from 'mongoose';
 
 let Schema = mongoose.Schema;
 
-let schema = new Schema({
+class GroupMemberSchema extends Schema {
+  constructor() {
+    super(arguments);
+
+    this.add({
+      enabled: {
+        type: Boolean,
+        default: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    });
+  }
+}
+
+let groupMemberSchema = new GroupMemberSchema();
+let groupMemberGroupSchema = new GroupMemberSchema({
+  group: {
+    type: Schema.Types.ObjectId,
+    ref: 'Group',
+    required: true
+  }
+});
+let groupMemberCommandSchema = new GroupMemberSchema({
+  command: {
+    type: Schema.Types.ObjectId,
+    ref: 'Command',
+    required: true
+  }
+});
+
+let groupSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -17,23 +50,30 @@ let schema = new Schema({
     type: Boolean,
     default: true
   },
-  group: {},
+  members: {
+    type: Schema.Types.ObjectId,
+    ref: 'GroupMember',
+    required: true
+  },
   queue: {
     type: String,
     required: true
   },
-  createdAt: Date
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-schema.pre('save', function(next) {
-  this.createdAt = new Date();
-  next();
-});
+groupSchema.index({queue: 1});
+groupSchema.index({creator: 1});
+groupSchema.index({enabled: 1});
+groupSchema.index({createdAt: -1});
+groupSchema.index({name: 1});
 
-schema.index({queue: 1});
-schema.index({creator: 1});
-schema.index({enabled: 1});
-schema.index({createdAt: -1});
-schema.index({name: 1});
-
-export let Group = db.model('Group', schema);
+export let GroupMember = db.model('GroupMember', groupMemberSchema);
+export let GroupMemberGroup = GroupMember.discriminator('GroupMemberGroup',
+  groupMemberGroupSchema);
+export let GroupMemberCommand = GroupMember.discriminator('GroupMemberCommand',
+  groupMemberCommandSchema);
+export let Group = db.model('Group', groupSchema);
